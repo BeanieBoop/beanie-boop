d0e6ce8b2b9b986cb14691a84ad3b073c0c7d031
Amanda Hussey <amandahussey@Amandas-MacBook-Air.local>
2018-01-09 17:06:12 -0600

    added default imgUrl to Product; added isEmail validation to User

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -14,6 +14,10 @@ const Product = db.define('product', {
   },
   inventoryQuantity: {
     type: Sequelize.INTEGER
+  },
+  imgUrl: {
+    type: Sequelize.STRING,
+    defaultValue: '/public/ty-logo.png'
   }
 })
 
