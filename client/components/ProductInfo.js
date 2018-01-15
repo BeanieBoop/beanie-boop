4af2fb3d37b258c547fc3560b6dba7afcba382d5
Shaun Hutch <shaunoff@Shauns-MBP.home>
2018-01-15 11:51:28 -0500

    fixed ProductInfo

--- client/components/ProductInfo.js
+++ client/components/ProductInfo.js
@@ -1,10 +1,12 @@
 import React from 'react';
 //import { fetchProducts } from '../store/products';
 import { connect } from 'react-redux';
-import { Label, Card, Loader } from 'semantic-ui-react';
+import { Label, Card, Loader , Segment,Header} from 'semantic-ui-react';
+import {ProductsCard} from "../components"
 
 function ProductInfo(props) {
-  const { product, reviews, isLoggedIn } = props;
+  const { product, reviews, isLoggedIn ,products} = props;
+  console.log("productInfo", props)
   const Review = (
     <a>
       <Label as="a" color="orange">
@@ -14,27 +16,51 @@ function ProductInfo(props) {
       </Label>
     </a>
   );
-
+  if(!products.length){
+    return <p>Loading....</p>
+  }
   return (
     // prettier-ignore
-    <div>
-    <Card
-      image={product.imgUrl}
-      name={product.name}
-      description={product.description}
-      price={product.price}
-    />
-    <div> {isLoggedIn ? <Review /> : <Loader active inline="centered" />}</div>
+    <div style={styles.wrapper}>
+      <ProductsCard product={product}/>
+      <div style={styles.info}>
+        <Segment>
+          <Header as='h3'>Description</Header>
+          {product.description}
+        </Segment>
+        <Segment>
+          <Header as='h3'>Reviews</Header>
+          {product.description}
+        </Segment>
+      </div>
+
+    {/* <div> {isLoggedIn ? <Review /> : <Loader active inline="centered" />}</div> */}
     </div>
   );
 }
-
+const styles={
+  wrapper: {
+    marginLeft: '30px',
+    display: 'flex'
+  },
+  info:{
+    marginTop: '20px',
+    marginRight: '40px',
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column'
+  }
+}
 const mapStateToProps = function(state, ownProps) {
-  const productId = +ownProps.match.params.productId;
+
+  const productId = +ownProps.match.params.id;
+
   return {
-    product: state.products.find(product => product.id === productId),
+    products: state.products.products,
+    product: state.products.products.find(product => product.id === productId),
   };
 };
 
-export default connect(mapStateToProps)(ProductInfo);
 
+
+export default connect(mapStateToProps)(ProductInfo);
