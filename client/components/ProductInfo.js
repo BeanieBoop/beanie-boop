0a0fa7222528f26affa45dbb3445817d246a7c6d
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2018-01-15 16:36:11 -0500

    Product reviews now part of product render

--- client/components/ProductInfo.js
+++ client/components/ProductInfo.js
@@ -1,58 +1,52 @@
 import React from 'react';
 //import { fetchProducts } from '../store/products';
 import { connect } from 'react-redux';
-import { Label, Card, Loader , Segment,Header} from 'semantic-ui-react';
-import {ProductsCard} from "../components"
+import { Segment, Header, Rating } from 'semantic-ui-react';
+import { ProductsCard } from '../components';
 
-function ProductInfo(props) {
-  const { product, reviews, isLoggedIn ,products} = props;
-  console.log("productInfo", props)
-  const Review = (
-    <a>
-      <Label as="a" color="orange">
-        {/*
-        Not sure how to load single review associated with current product
-        */}
-      </Label>
-    </a>
-  );
-  if(!products.length){
-    return <p>Loading....</p>
-  }
-  return (
-    // prettier-ignore
-    <div style={styles.wrapper}>
-      <ProductsCard product={product}/>
-      <div style={styles.info}>
-        <Segment>
-          <Header as='h3'>Description</Header>
-          {product.description}
-        </Segment>
-        <Segment>
-          <Header as='h3'>Reviews</Header>
-          {product.description}
-        </Segment>
-      </div>
-
-    {/* <div> {isLoggedIn ? <Review /> : <Loader active inline="centered" />}</div> */}
-    </div>
-  );
-}
-const styles={
+const styles = {
   wrapper: {
     marginLeft: '30px',
-    display: 'flex'
+    display: 'flex',
   },
-  info:{
+  info: {
     marginTop: '20px',
     marginRight: '40px',
     flex: 1,
     display: 'flex',
-    flexDirection: 'column'
-  }
-}
-const mapStateToProps = function(state, ownProps) {
+    flexDirection: 'column',
+  },
+};
 
+function ProductInfo(props) {
+  const { product, products } = props;
+  console.log('product', product);
+
+  if (!products.length) {
+    return <p>Loading....</p>;
+  }
+  return (
+    <div style={styles.wrapper}>
+      <ProductsCard product={product} />
+      <div style={styles.info}>
+        <Segment>
+          <Header as="h3">Description</Header>
+          {product.description}
+        </Segment>
+        <Segment>
+          <Header as="h3">Reviews</Header>
+          {product.reviews.map(review => (
+            <div key={review.id}>
+              {<Rating icon="heart" defaultRating={review.rating} maxRating={5} />} {review.reviewText}
+            </div>
+          ))}
+        </Segment>
+      </div>
+    </div>
+  );
+}
+
+const mapStateToProps = function(state, ownProps) {
   const productId = +ownProps.match.params.id;
 
   return {
@@ -60,7 +54,5 @@ const mapStateToProps = function(state, ownProps) {
     product: state.products.products.find(product => product.id === productId),
   };
 };
-
-
 
 export default connect(mapStateToProps)(ProductInfo);
